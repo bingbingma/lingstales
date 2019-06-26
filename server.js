937e928ba0c17e8b9c0bb946045e9981a563dca3
David Ma <7047982+bingbingma@users.noreply.github.com>
2019-06-25 21:32:10 -0400

    Uncomment server code

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.get("/ping", function (req, res) {
 app.use(routes);
 
 // Send every other request to the React app
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
