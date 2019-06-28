eb75a001586a2ead7d69da72ca3fd10d2a8abd58
David Ma <7047982+bingbingma@users.noreply.github.com>
2019-06-27 22:38:22 -0400

    resized comment list

--- client/src/components/commentmaster.js
+++ client/src/components/commentmaster.js
@@ -65,7 +65,7 @@ class Comments_Section extends Component {
           </div>
         </div>
         <div className="row">
-          <div className="col-12 col-md-8 col-lg-8 pt-3 bg-white">
+          <div className="col-12 col-md-6 col-lg-8 pt-3 bg-white">
             <CommentList
               loading={this.state.loading}
               comments={this.state.comments}
